cfa8b55979f19ff722ba16592c758272b89f684f
jyotipatidar20 <jyotipatidar20@navgurukul.org>
2021-11-11 15:12:42 +0530

    first commit

---
+++ question.3.exercise.py
@@ -0,0 +1,4 @@
+my_file=open("bank_list","w")
+my_file.write("kotak\nHDFC\nRBI\nSBI\nBank of Baroda")
+print(my_file)
+my_file.close
---
+++ question.4.exercise.py
@@ -0,0 +1,20 @@
+my_file=open("delhi.txt","w")
+my_file.write("imtiyaz - delhi\nayishah - delhi\nkarthikeyan - delhi\nsiddhi - delhi\nmohinder - delhi\nneela - delhi\nsarika - delhi\nharshad - delhi\nsekhar - delhi\nanoop - delhi\npankaj - delhi\nbrijesh - delhi\ngovind - delhi\nnand - delhi")
+print(my_file)
+my_file.close()
+
+
+
+my_file=open("shimla.txt","w")
+my_file.write("rati - shimla\nraghu - shimla\npuneet - shimla\nrajeev - shimla\ndeepti - shimla\n")
+print(my_file)
+my_file.close()
+
+
+my_file=open("others.txt","w")
+my_file.write("rishabh - meerut\nnilima - cochin\nraghu - shimla\nnaseer - kanpur\nsalma - jaipur\nsuman - jaipur\nrajendra - jaipur\nsashi - jaipur\ntrishna - raipur\npradeep - jaipur\nbalwinder - tokyo")
+print(my_file)
+my_file.close()
+
+
+
